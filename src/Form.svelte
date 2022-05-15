50c2e56266638c200e52581af7be1a0574b134a9
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-15 17:25:23 +0300

    Finished the form component and got add/cancel buttons working

--- src/Form.svelte
+++ src/Form.svelte
@@ -1,1 +1,49 @@
-<h1>Form</h1>
+<script>
+    export let id;
+    export let name = "";
+    export let price;
+
+    $: mode = id ? 'Edit' : 'Add';
+    $: canSubmit = price >= 0 && name !== '';
+    
+    function submit() {
+        if (!canSubmit) {
+            return;
+        }
+
+        price = '';
+        name = '';
+        id = undefined;
+    }
+
+    function cancel() {
+        price = '';
+        name = '';
+        id = undefined;
+    }
+</script>
+
+<style>
+    button {
+        margin-left: 20px;
+    }
+
+    button:disabled {
+        cursor: not-allowed;
+    }
+</style>
+
+<form on:submit|preventDefault={submit}>
+    <fieldset>
+        <label for="nameField">Material</label>
+        <input bind:value={name} placeholder="Wood, Glue, Etc" type="text" id="nameField" />
+
+        <label for="priceField">Price</label>
+        <input bind:value={price} min="0" step="any" placeholder="Price" type="number" id="priceField" />
+    </fieldset>
+    <button disabled={!canSubmit} class="float-right" type="submit">{mode}</button>
+
+    {#if mode === 'Edit'}
+        <button on:click={cancel} class="float-right" type="button">Cancel</button>
+    {/if}
+</form>
